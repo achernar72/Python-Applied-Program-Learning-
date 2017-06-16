052a4bdff4055adff7c5fafe94aafcaa8c960689
achernar72 <sankarasettyavinash@gmail.com>
2017-06-16 12:30:38 -0500

    Add files via upload

---
+++ Class2/CharFrequency.py
@@ -0,0 +1,5 @@
+word = input("Enter a word that has find the frequency:")
+
+Alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for i in range(0,26):
+    print(Alphabet[i]," ",word.count(Alphabet[i]))
